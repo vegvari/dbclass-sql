4efebfcda8e69f22bbb839f985d2a86109d1e9d1
Romeo Vegvari <vegvari@users.noreply.github.com>
2019-03-07 21:31:56 +0000

    Charset and collation removed from Create class

--- src/MySQL/Create.php
+++ src/MySQL/Create.php
@@ -4,13 +4,13 @@ namespace DBClass\SQL\MySQL;
 
 abstract class Create
 {
-    public static function database(string $name, string $charset = null, string $collation = null): Interfaces\CreateDatabase
+    public static function database(string $name): Interfaces\CreateDatabase
     {
-        return new CreateDatabase($name, $charset, $collation);
+        return new CreateDatabase($name);
     }
 
-    public static function table(string $name, string $charset = null, string $collation = null): Interfaces\CreateTable
+    public static function table(string $name): Interfaces\CreateTable
     {
-        return new CreateTable($name, $charset, $collation);
+        return new CreateTable($name);
     }
 }
