0f621b5d1c12220cc6140974613dd75c3235a362
Romeo Vegvari <vegvari@users.noreply.github.com>
2019-03-23 13:02:20 +0000

    Factory tests

---
+++ test/MySQL/FactoryTest.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace DBClass\SQL\MySQL;
+
+use PHPUnit\Framework\TestCase;
+
+class FactoryTest extends TestCase
+{
+    public function test()
+    {
+        $this->assertEquals(Create::database('foo'), new CreateDatabase('foo'));
+        $this->assertEquals(Create::table('foo'), new CreateTable('foo'));
+        $this->assertEquals(Drop::database('foo'), new DropDatabase('foo'));
+        $this->assertEquals(Drop::table('foo'), new DropTable('foo'));
+    }
+}
