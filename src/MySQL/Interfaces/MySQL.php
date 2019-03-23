b8cf518b0a7fbb005d33a2e1f806ef82b196a747
Romeo Vegvari <vegvari@users.noreply.github.com>
2019-03-23 12:00:58 +0000

    MySQL interface for default values

---
+++ src/MySQL/Interfaces/MySQL.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace DBClass\SQL\MySQL\Interfaces;
+
+interface MySQL
+{
+    const DEFAULT_ENGINE = 'InnoDB';
+    const DEFAULT_CHARSET = 'utf8mb4';
+    const DEFAULT_COLLATION = 'utf8mb4_unicode_ci';
+}
