7ba99f6e0c69375ec1afc99cb7311e3e69ced490
Romeo Vegvari <vegvari@users.noreply.github.com>
2019-03-29 07:51:45 +0000

    Database test

--- test/MySQL/CreateDatabaseTest.php
+++ test/MySQL/CreateDatabaseTest.php
@@ -6,6 +6,8 @@ use PHPUnit\Framework\TestCase;
 
 class CreateDatabaseTest extends TestCase
 {
+    use Connection;
+
     public function getImplementations(): array
     {
         return [
@@ -16,48 +18,28 @@ class CreateDatabaseTest extends TestCase
     /**
      * @dataProvider getImplementations
      */
-    public function test_name(callable $obj)
+    public function test(callable $obj)
     {
         $obj = $obj('foo');
-        $this->assertSame('CREATE DATABASE `foo` CHARACTER SET `utf8mb4` COLLATE `utf8mb4_unicode_ci`;', $obj->getBuild());
 
-        $obj->setName('bar');
-        $this->assertSame('CREATE DATABASE `bar` CHARACTER SET `utf8mb4` COLLATE `utf8mb4_unicode_ci`;', $obj->getBuild());
+        $this->exec(Drop::database('foo')->ifExists());
+        $this->exec($obj);
+        $this->assertSame($obj->getBuild(), $this->showCreateDatabase('foo'));
+        $this->exec(Drop::database('foo'));
     }
 
     /**
      * @dataProvider getImplementations
      */
-    public function test_if_not_exists(callable $obj)
+    public function test_charset_collation(callable $obj)
     {
         $obj = $obj('foo');
-        $this->assertSame('CREATE DATABASE `foo` CHARACTER SET `utf8mb4` COLLATE `utf8mb4_unicode_ci`;', $obj->getBuild());
+        $obj->setCharset('latin1');
+        $obj->setCollation('latin1_bin');
 
-        $obj->setIfNotExists(true);
-        $this->assertSame('CREATE DATABASE IF NOT EXISTS `foo` CHARACTER SET `utf8mb4` COLLATE `utf8mb4_unicode_ci`;', $obj->getBuild());
-    }
-
-    /**
-     * @dataProvider getImplementations
-     */
-    public function test_charset(callable $obj)
-    {
-        $obj = $obj('foo');
-        $this->assertSame('CREATE DATABASE `foo` CHARACTER SET `utf8mb4` COLLATE `utf8mb4_unicode_ci`;', $obj->getBuild());
-
-        $obj->setCharset('bar');
-        $this->assertSame('CREATE DATABASE `foo` CHARACTER SET `bar` COLLATE `utf8mb4_unicode_ci`;', $obj->getBuild());
-    }
-
-    /**
-     * @dataProvider getImplementations
-     */
-    public function test_collation(callable $obj)
-    {
-        $obj = $obj('foo');
-        $this->assertSame('CREATE DATABASE `foo` CHARACTER SET `utf8mb4` COLLATE `utf8mb4_unicode_ci`;', $obj->getBuild());
-
-        $obj->setCollation('bar');
-        $this->assertSame('CREATE DATABASE `foo` CHARACTER SET `utf8mb4` COLLATE `bar`;', $obj->getBuild());
+        $this->exec(Drop::database('foo')->ifExists());
+        $this->exec($obj);
+        $this->assertSame($obj->getBuild(), $this->showCreateDatabase('foo'));
+        $this->exec(Drop::database('foo'));
     }
 }
