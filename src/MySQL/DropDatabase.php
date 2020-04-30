3b77df95e318620e71c076f368ecdb17aa00bdc8
Romeo Vegvari <vegvari@users.noreply.github.com>
2020-04-30 14:17:00 +0100

    Database and table drop finalized

--- src/MySQL/DropDatabase.php
+++ src/MySQL/DropDatabase.php
@@ -2,38 +2,38 @@
 
 namespace DBClass\MySQL;
 
-class DropDatabase implements Interfaces\DDLStatement
+final class DropDatabase implements Interfaces\DDLStatement
 {
     private $databaseName;
     private $ifExists = false;
 
-    final public function __construct(string $databaseName)
+    public function __construct(string $databaseName)
     {
         $this->databaseName = $databaseName;
     }
 
-    final public function getDatabaseName(): string
+    public function getDatabaseName(): string
     {
         return $this->databaseName;
     }
 
-    final public function getName(): string
+    public function getName(): string
     {
         return $this->databaseName;
     }
 
-    final public function setIfExists(bool $value = true): self
+    public function setIfExists(bool $value = true): self
     {
         $this->ifExists = $value;
         return $this;
     }
 
-    final public function getIfExists(): bool
+    public function getIfExists(): bool
     {
         return $this->ifExists;
     }
 
-    final public function getBuild(): string
+    public function getBuild(): string
     {
         $build[] = 'DROP DATABASE';
 
--- src/MySQL/DropTable.php
+++ src/MySQL/DropTable.php
@@ -2,55 +2,55 @@
 
 namespace DBClass\MySQL;
 
-class DropTable implements Interfaces\DDLStatement
+final class DropTable implements Interfaces\DDLStatement
 {
     private $databaseName;
     private $tableName;
     private $ifExists = false;
 
-    final public function __construct(string $tableName)
+    public function __construct(string $tableName)
     {
         $this->tableName = $tableName;
     }
 
-    final public function setDatabaseName(string $databaseName): self
+    public function setDatabaseName(string $databaseName): self
     {
         $this->databaseName = $databaseName;
         return $this;
     }
 
-    final public function getDatabaseName(): string
+    public function getDatabaseName(): string
     {
         return $this->databaseName;
     }
 
-    final public function hasDatabaseName(): bool
+    public function hasDatabaseName(): bool
     {
         return $this->databaseName !== null;
     }
 
-    final public function getTableName(): string
+    public function getTableName(): string
     {
         return $this->tableName;
     }
 
-    final public function getName(): string
+    public function getName(): string
     {
         return $this->tableName;
     }
 
-    final public function setIfExists(bool $value = true): self
+    public function setIfExists(bool $value = true): self
     {
         $this->ifExists = $value;
         return $this;
     }
 
-    final public function getIfExists(): bool
+    public function getIfExists(): bool
     {
         return $this->ifExists;
     }
 
-    final public function getBuild(): string
+    public function getBuild(): string
     {
         $build[] = 'DROP TABLE';
 
